14e331cbf846d34368871993435fe39c7c9f520d
GaddamSaiKrishna <gaddamsaikrishna460@gmail.com>
2024-07-31 12:17:05 +0530

    upd:api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "be21e9633d71492eb26ed4e8d567a77d";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
